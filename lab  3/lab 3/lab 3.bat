0665de78efb2bc8185a7fac79c920e59d0575bb9
Pavel <144237207+AtOmMEN@users.noreply.github.com>
2024-05-03 15:20:54 +0300

    Delete lab  3 directory

--- lab  3/lab 3/lab 3.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-mkdir "Not hidden folder"
-mkdir "Hidden folder"
-attrib +h "Hidden folder"
-xcopy /? > "Not hidden folder\copyhelp.txt"
-xcopy "Not hidden folder\copyhelp.txt" "Hidden folder\copied_copyhelp.txt"
