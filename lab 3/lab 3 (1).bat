8be0df577fed463fa681d709ff8aa4713f2e1ac3
Pavel <144237207+AtOmMEN@users.noreply.github.com>
2024-09-17 16:57:16 +0300

    Delete lab 3 directory

--- lab 3/lab 3 (1).bat
+++
@@ -1,7 +0,0 @@
-@echo off
-mkdir "PT-23-1\Dovzhenko P.G\batch"
-mkdir "PT-23-1\Dovzhenko P.G\batch\Not hidden folder"
-mkdir "PT-23-1\Dovzhenko P.G\batch\Hidden folder"
-attrib +h "PT-23-1\Dovzhenko P.G\batch\Hidden folder"
-xcopy /? > "PT-23-1\Dovzhenko P.G\batch\Not hidden folder\copytext.txt"
-xcopy "PT-23-1\Dovzhenko P.G\batch\Not hidden folder\copytext.txt" "PT-23-1\Dovzhenko P.G\batch\Hidden folder\copied_copytext.txt"
